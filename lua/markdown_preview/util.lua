9f67bd3007eeee7c3ae95c986c56e0e425b94abb
Nelson Lee <leelhn2345@outlook.com>
2026-04-15 11:02:12 +0800

    change to explorer.exe

--- lua/markdown_preview/util.lua
+++ lua/markdown_preview/util.lua
@@ -74,7 +74,7 @@ function M.open_in_browser(url)
 	if vim.fn.has("mac") == 1 then
 		cmd = { "open", url }
 	elseif vim.fn.has("wsl") == 1 then
-		cmd = { "wslview", url }
+		cmd = { "explorer.exe", url }
 	elseif vim.fn.has("unix") == 1 then
 		cmd = { "xdg-open", url }
 	elseif vim.fn.has("win32") == 1 then
